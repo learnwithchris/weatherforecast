044b95b8f929e4d1f9c12be0995d99f25f7cc6a5
learnwithchris <56309725+learnwithchris@users.noreply.github.com>
2019-11-15 15:01:55 -0500

    Updates

---
+++ precache-manifest.46721bd7a3fc69a36cdaf9d66009a321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfb3d2f08f5bd1ccf8d20c2fcc236d7",
+    "url": "/weatherforecast/index.html"
+  },
+  {
+    "revision": "f430780256e772afe780",
+    "url": "/weatherforecast/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "05e34778f00b66b704f0",
+    "url": "/weatherforecast/static/css/main.1b685f78.chunk.css"
+  },
+  {
+    "revision": "f430780256e772afe780",
+    "url": "/weatherforecast/static/js/2.f16cd229.chunk.js"
+  },
+  {
+    "revision": "05e34778f00b66b704f0",
+    "url": "/weatherforecast/static/js/main.63f59193.chunk.js"
+  },
+  {
+    "revision": "570b19f342f63e06df58",
+    "url": "/weatherforecast/static/js/runtime-main.bb57bb96.js"
+  }
+]);
